c6a8d34917c99675e72a0cb6ea58a7bb1b189384
miles-lenz <miles@lenz-be.de>
2025-04-04 10:33:07 +0200

    update routing with History API

--- js/main.js
+++ js/main.js
@@ -1,28 +1,40 @@
+const LINKS = [
+    { id: "#link-main", page: "main" },
+    { id: "#link-page1", page: "page1" },
+    { id: "#link-page2", page: "page2" },
+    { id: "#link-legal-notice", page: "legal-notice" },
+    { id: "#link-privacy-policy", page: "privacy-policy" },
+];
+
+
 function loadPage(page) {
 
     fetch(`pages/${page}.html`)
         .then(response => response.text())
-        .then(data => document.querySelector("main").innerHTML = data)
-        .catch(error => console.error("", error));
+        .then(data => {
+            document.querySelector("main").innerHTML = data;
+            if (history.state && history.state.page !== page) {
+                const url = page == "main" ? "/" : `/${page}`;
+                history.pushState({ page: page }, "", url);
+            }
+        })
+        .catch(error => console.error(error));
 }
 
 
 function main() {
 
+    history.replaceState({ page: "main" }, "", "/");
     loadPage("main");
 
-    const links = [
-        { id: "#link-main", page: "main" },
-        { id: "#link-page1", page: "page1" },
-        { id: "#link-page2", page: "page2" },
-        { id: "#link-legal-notice", page: "legal-notice" },
-        { id: "#link-privacy-policy", page: "privacy-policy" },
-    ];
+    window.addEventListener('popstate', (event) => {
+        loadPage(event.state.page);
+    });
 
-    links.forEach(link => {
-        const element = document.querySelector(link.id)
-        element.addEventListener("click", function (e) {
-            e.preventDefault();
+    LINKS.forEach(link => {
+        const element = document.querySelector(link.id);
+        element.addEventListener("click", function (event) {
+            event.preventDefault();
             loadPage(link.page);
         });
     });
