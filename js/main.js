c55bb6de3442cafb5749ca89ffeafeda5e7302b2
miles-lenz <miles@lenz-be.de>
2025-05-10 11:48:56 +0200

    fix map authentication error

--- js/main.js
+++ js/main.js
@@ -6,12 +6,10 @@ function loadPage(oldPage, newPage) {
         .then(data => {
             document.querySelector("main").innerHTML = data;
             if (newPage == "main") {
-                const map = L.map("cu-map").setView([50.433446, 8.078516], 11.8);
-                L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.{ext}', {
-                    minZoom: 0,
-                    maxZoom: 20,
-                    attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-                    ext: 'png'
+                const map = L.map("cu-map").setView([50.433446, 8.078516], 12.8);
+                L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                    maxZoom: 19,
+                    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
                 }).addTo(map);
                 L.marker([50.433446, 8.078516]).addTo(map);
             }
