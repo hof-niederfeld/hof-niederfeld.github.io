3b0edaac542888a7cf74b44d2dc42086889834c5
miles-lenz <miles@lenz-be.de>
2025-05-20 19:49:23 +0200

    add initial layout/content for main page

--- js/main.js
+++ js/main.js
@@ -3,7 +3,17 @@ function loadPage(newPage) {
     // Update the page content.
     fetch(`pages/${newPage}.html`)
         .then(response => response.text())
-        .then(data => document.querySelector("main").innerHTML = data)
+        .then(data => {
+            document.querySelector("main").innerHTML = data;
+            if (newPage == "home") {
+                const map = L.map("cu-map").setView([50.433446, 8.078516], 12.8);
+                L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                    maxZoom: 19,
+                    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+                }).addTo(map);
+                L.marker([50.433446, 8.078516]).addTo(map);
+            }
+        })
         .catch(error => console.error(error));
 
     // Update the page title.
