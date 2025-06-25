add88d9340cf761105508c8be50cfdfbd452ea19
miles-lenz <miles@lenz-be.de>
2025-06-25 13:30:58 +0200

    improve lohnarbeit page

--- assets/js/lohnarbeit.js
+++ assets/js/lohnarbeit.js
@@ -1,19 +1,19 @@
 function initMap() {
 
-    const map = L.map("cu-map").setView([50.433446, 8.078516], 11);
+    const map = L.map("cu-map").setView([50.433446, 8.078516], 9.3);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
 
-    L.marker([50.433446, 8.078516]).addTo(map);
+    // L.marker([50.433446, 8.078516]).addTo(map);
 
     L.circle([50.433446, 8.078516], {
-        color: '#aa0000',
-        fillColor: '#f03',
+        color: '#007BFF99',
+        fillColor: '#007BFF',
         fillOpacity: 0.2,
-        radius: 5500
+        radius: 30000
     }).addTo(map);
 }
 
